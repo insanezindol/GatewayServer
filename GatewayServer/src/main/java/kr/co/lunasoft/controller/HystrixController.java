6dc5ff1638be517f4eabb3a48f122e5a86bb34f6
딘 <jhlee@lunasoft.co.kr>
2020-01-14 16:27:52 +0900

    add hystrix call

--- GatewayServer/src/main/java/kr/co/lunasoft/controller/HystrixController.java
+++ GatewayServer/src/main/java/kr/co/lunasoft/controller/HystrixController.java
@@ -2,15 +2,17 @@ package kr.co.lunasoft.controller;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
+import kr.co.lunasoft.vo.ResponseInfo;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -18,22 +20,26 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class HystrixController {
 
-	@GetMapping(value = "/test")
-	@HystrixCommand(fallbackMethod = "fallback", commandProperties = { @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000") })
-	public Map<String, Object> test() {
+	@Autowired
+	RestTemplate restTemplate;
 
-		int rand = new Random().nextInt(5) + 2;
-		try {
-			Thread.sleep(rand * 1000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		log.info("sec : " + rand);
+	@GetMapping(value = "/test")
+	@HystrixCommand(fallbackMethod = "fallback", commandProperties = {
+			   @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000"),
+			   @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "10000"),
+			   @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "10"),
+			   @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+			   @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000")})
+	public Map<String, Object> test() {
+		String reqUrl = "http://eureka-client/client/long-time";
+
+		ResponseInfo output = restTemplate.getForObject(reqUrl, ResponseInfo.class);
+		log.info(""+output);
 
 		Map<String, Object> obj = new HashMap<String, Object>();
 		obj.put("code", "100200");
 		obj.put("msg", "success");
-		obj.put("data", "");
+		obj.put("data", output);
 		return obj;
 	}
 
